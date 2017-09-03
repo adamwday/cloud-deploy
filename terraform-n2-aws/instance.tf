0e262d35265e93b59648e252dc0f582b5b955af5
adamwday <adam.day@outlook.com>
2017-09-03 13:53:42 -0600

    Adding new config files for Terraform deploys.

--- terraform-n2-aws/instance.tf
+++ terraform-n2-aws/instance.tf
@@ -1,7 +1,7 @@
 # Instance file used to change the AMI id into a lookup into a map variable
 # which is stored in the vars.tf file.
 
-resource "aws_instance" "example instance" {
+resource "aws_instance" "example" {
             ami = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
 }
