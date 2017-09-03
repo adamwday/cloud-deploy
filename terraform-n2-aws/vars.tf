1127da2ad42027589d88b4a2e91024790336ced9
adamwday <adam.day@outlook.com>
2017-09-03 15:09:19 -0600

    Adding additional files for Terraform deployment.

--- terraform-n2-aws/vars.tf
+++ terraform-n2-aws/vars.tf
@@ -13,3 +13,13 @@ variable "AWS_REGION" {
     us-east-2 = "ami-dbbd9dbe"
     }
   }
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
+}
