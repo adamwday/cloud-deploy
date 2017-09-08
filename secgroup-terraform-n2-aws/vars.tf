f4e0ca2e7a0e2dc5418b45cbd6686a44d32e1e3b
adamwday <adam.day@outlook.com>
2017-09-07 20:50:47 -0600

    Adding additional code comment and cleanup.

--- secgroup-terraform-n2-aws/vars.tf
+++ secgroup-terraform-n2-aws/vars.tf
@@ -1,5 +1,10 @@
-# Used to declare the variables we want to use.
-
+/*
+********************************************************
+Declares variables for looking up the aws access key,
+secret key, and default region as referenced in main.tf.
+This is done so that we do not publicly expose the keys.
+********************************************************
+*/
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
