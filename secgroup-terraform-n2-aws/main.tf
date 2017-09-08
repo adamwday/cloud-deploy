48967eaeaddbea6c239936c27c22ba989a616f12
adamwday <adam.day@outlook.com>
2017-09-07 20:46:00 -0600

    Updating code comments.

--- secgroup-terraform-n2-aws/main.tf
+++ secgroup-terraform-n2-aws/main.tf
@@ -1,9 +1,20 @@
+/*
+****************************************************
+Looks up access key, secret key, and default region
+from terraform.tfvars (in gitignore list).
+****************************************************
+*/
 provider "aws" {
 access_key = "${var.AWS_ACCESS_KEY}"
 secret_key = "${var.AWS_SECRET_KEY}"
     region = "${var.AWS_REGION}"
 }
 
+/*
+****************************************************
+Creates security group & adds rules.
+****************************************************
+*/
   resource "aws_security_group" "allow_ssh" {
   name = "allow_all"
   description = "Allow inbound SSH traffic from my IP"
