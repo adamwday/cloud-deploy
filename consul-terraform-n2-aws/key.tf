9df9af5815e53d905032469f961360756afd3fc8
adamwday <adam.day@outlook.com>
2017-09-09 22:05:41 -0600

    .

--- consul-terraform-n2-aws/key.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "mykeyubu" {
-  key_name = "mykeyubu"
-  public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
-}
--- ubuntu-terraform-n2-aws/output.tf
+++
@@ -1,6 +0,0 @@
-output "jenkins-ip" {
-  value = "${aws_instance.jenkins.public_ip}"
-}
-output "app-ip" {
-  value = "${aws_instance.app1.public_ip}"
-}
