400c09dc4e2be3095f30962d83d45b16e4a5884b
adamwday <adam.day@outlook.com>
2017-09-09 23:21:07 -0600

    .

--- ubuntu-terraform-n2-aws/backend.tf
+++ ubuntu-terraform-n2-aws/backend.tf
@@ -1,12 +1,12 @@
-/*
-*****************************************
-Stores the Terraform state file into S3.
-*****************************************
-*/
-
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-wd40"
-    key    = "terraform/ubuntu-terraform-n2-aws"
-  }
-}
+#/*
+#*****************************************
+#Stores the Terraform state file into S3.
+#*****************************************
+#*/
+#
+#terraform {
+#  backend "s3" {
+#    bucket = "terraform-sytate-wd40"
+#    key    = "terraform/ubuntu-terraform-n2-aws"
+#  }
+#}
--- ubuntu-terraform-n2-aws/output.tf
+++ ubuntu-terraform-n2-aws/output.tf
@@ -1,6 +1,6 @@
-output "jenkins-ip" {
+output "your jenkins public ip" {
   value = "${aws_instance.jenkins.public_ip}"
 }
-output "app-ip" {
+output "your app1 public ip" {
   value = "${aws_instance.app1.public_ip}"
 }
