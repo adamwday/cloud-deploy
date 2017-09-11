8f2b62429a9b37835d91158cac6905869763ffdb
adamwday <adam.day@outlook.com>
2017-09-10 22:41:32 -0600

    Updating output file for docker images.

--- ubuntu-terraform-n2-aws/output.tf
+++ ubuntu-terraform-n2-aws/output.tf
@@ -8,6 +8,7 @@ been run on a given instance. (public IP, etc.)
 output "your jenkins public ip" {
   value = "${aws_instance.jenkins.public_ip}"
 }
+
 output "your app1 public ip" {
   value = "${aws_instance.app1.public_ip}"
 }
