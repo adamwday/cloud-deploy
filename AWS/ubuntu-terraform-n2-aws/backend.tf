d4d9f9ae8c15867cfbc2aa704569bb36ac5b6aa5
adamwday <adam.day@outlook.com>
2017-09-20 21:35:43 -0600

    Updating remote tfstate data

--- AWS/ubuntu-terraform-n2-aws/backend.tf
+++ AWS/ubuntu-terraform-n2-aws/backend.tf
@@ -6,7 +6,7 @@ Stores the Terraform state file into S3.
 
 terraform {
   backend "s3" {
-    bucket = "terraform-state-wd40"
+    bucket = "s3-tfstate-wd40"
     key    = "terraform/ubuntu-terraform-n2-aws"
     region = "us-east-2"
   }
