7dc0b43d01af859316aa03df9c931e5cde02ec79
Adam Day <adam.day@outlook.com>
2018-04-08 16:40:29 -0700

    Clearning up terraform modules

--- AWS/us-east-2/consul-terraform-n2-aws/modules.tf
+++
@@ -1,9 +0,0 @@
-module "consul" {
-  source = "https://github.com/adamwday/consul-terraform-module.git"
-  key_name = "${aws_key_pair.mykeyubu.key_name}"
-  key_path = "${var.PATH_TO_PRIVATE_KEY}"
-  region = "${var.AWS_REGION}"
-}
-output "consul-output" {
-  value = "${module.consul.server_address}"
-}
